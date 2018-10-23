9a82e7b695f528a6b247c4f6942d7a3726ea5278
takehanKosuke <kkoussuke530@icloud.com>
2018-10-24 00:55:48 +0900

    create sign_up & sign_in views

--- db/migrate/20181023144654_devise_create_users.rb
+++ db/migrate/20181023144654_devise_create_users.rb
@@ -19,11 +19,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :image, null: false, default: ""
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
