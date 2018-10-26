f172607d547660e826fc4e7ee71b6d69820cda39
takehanKosuke <kkoussuke530@icloud.com>
2018-10-26 14:35:17 +0900

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,76 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+  name: 'test1',
+  email: 'test1@test.com',
+  image: File.open("./public/images/men1.jpg"),
+  password: '111111'
+)
+
+User.create(
+  name: 'test2',
+  email: 'test2@test.com',
+  image: File.open("./public/images/men2.jpg"),
+  password: '111111'
+)
+
+User.create(
+  name: 'test3',
+  email: 'test3@test.com',
+  image: File.open("./public/images/men3.jpg"),
+  password: '111111'
+)
+
+User.create(
+  name: 'tes4',
+  email: 'test4@test.com',
+  image: File.open("./public/images/women1.jpg"),
+  password: '111111'
+)
+
+User.create(
+  name: 'tes5',
+  email: 'test5@test.com',
+  image: File.open("./public/images/women2.jpg"),
+  password: '111111'
+)
+
+Room.create(
+  last_message_at: '2018-10-26 13:00:00'
+)
+
+Room.create(
+  last_message_at: '2018-10-26 14:00:00'
+)
+
+Room.create(
+  last_message_at: '2018-10-26 15:00:00'
+)
+
+UserRoom.create(
+  user_id: 1,
+  room_id: 1
+)
+
+UserRoom.create(
+  user_id: 2,
+  room_id: 1
+)
+
+UserRoom.create(
+  user_id: 1,
+  room_id: 2
+)
+
+UserRoom.create(
+  user_id: 3,
+  room_id: 2
+)
+
+UserRoom.create(
+  user_id: 1,
+  room_id: 3
+)
+
+UserRoom.create(
+  user_id: 4,
+  room_id: 3
+)
