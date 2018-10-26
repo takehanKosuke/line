b824d4bbffd59d7923b72c5b36cfa95630a3ceef
takehanKosuke <kkoussuke530@icloud.com>
2018-10-26 17:53:27 +0900

    tweak application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :move_to_login , only: [:user]
+  before_action :authenticate_user!
 
   private
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
-    end
-
-    def move_to_login
-      redirect_to new_user_session_path unless user_signed_in?
     end
 end
