5067d4defb9b09e3520e124e4cd5bc8320e22755
takehanKosuke <kkoussuke530@icloud.com>
2018-10-27 12:48:10 +0900

    tweak

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,4 +4,25 @@ class RoomsController < ApplicationController
     @room = Room.find(params[:id])
     @messages = @room.messages.all.order(created_at: :desc)
   end
+
+
+
+    def create
+      binding.pry
+      @room = Room.new(room_params)
+      # binding.pry
+      if @room.save
+        redirect_to room_path(@room.id)
+      else
+        redirect_to root_path
+      end
+    end
+
+    private
+    def room_params
+      params.require(:room).permit(
+        :last_message_at,
+        {user_room_ids: []}
+      )
+    end
 end
