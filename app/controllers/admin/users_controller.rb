b286765a94bd0d1a888af85e2e7e4c5d230109f3
takehanKosuke <kkoussuke530@icloud.com>
2018-10-29 18:47:10 +0900

    create mini admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -17,15 +17,19 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def update
-    current_user.update(user_params)
-    redirect_to admin_root_path
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admin_users_path
   end
 
-  def delete
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
   end
 
   private
-  def update_params
+  def user_params
     params.require(:user).permit(:image, :name)
   end
 
