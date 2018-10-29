2e547dad1468efe1ea7361c4d02dcd888d83852f
takehanKosuke <kkoussuke530@icloud.com>
2018-10-30 08:46:52 +0900

    tweak

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,9 +2,15 @@ class RoomsController < ApplicationController
   def show
     @user = current_user
     @room = Room.find(params[:id])
-    @messages = @room.messages.all.order(created_at: :desc)
-    @messages.where.not(user_id: @user).each do |message|
-      message.update(is_read: true)
+    # roomの参加者以外の人がURLベタ打ちでroomに入ってきたときにトップにリダイレクトさせる
+    if @room.users.any?{|u| u == @user}
+      @messages = @room.messages.all.order(created_at: :desc)
+      # メッセージを既読状態にする
+      @messages.where.not(user_id: @user).each do |message|
+        message.update(is_read: true)
+      end
+    else
+      redirect_to root_path
     end
   end
 
