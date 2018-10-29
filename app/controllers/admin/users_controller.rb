6207116a8294d90b2ca28ebb1fdf0149c4a9d720
takehanKosuke <kkoussuke530@icloud.com>
2018-10-30 08:05:02 +0900

    created search user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,7 @@
 class Admin::UsersController < Admin::BaseController
   def index
-    @users = User.all
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true)
   end
 
   def show
